a1904bffdd16fed608d223301d1c167e0650a0c9
LXing <31298876+FlyOrBoom@users.noreply.github.com>
2020-05-20 13:53:19 -0700

    added thx

--- bot/src/config/config.ts
+++ bot/src/config/config.ts
@@ -60,5 +60,5 @@ export const config: BotConfig = {
     '664280781509885972': 5,
     'thanks': 10,
   },
-  thanksKeywords: ["thanks", "thank", "thunk", "kudos"],
+  thanksKeywords: ["thx", "thank", "thunk", "kudos"],
 };
