c90b47c35831eaa2f280150aae2751d4131a9523
Gabe Dunn <gabe@gabedunn.dev>
2021-01-09 08:54:36 +0100

    Author on embed

--- bot/src/commands/top.ts
+++ bot/src/commands/top.ts
@@ -13,12 +13,12 @@ export class TopCommand implements Command {
 
   async run(parsedUserCommand: CommandContext): Promise<void> {
     const args = parsedUserCommand.args;
-    if(args.length > 0){
+    if (args.length > 0) {
       this.sendLeaderboard(parsedUserCommand.originalMessage, parseInt(args[0]));
     } else {
-      this.sendLeaderboard(parsedUserCommand.originalMessage,  7);
+      this.sendLeaderboard(parsedUserCommand.originalMessage, 7);
     }
-    }
+  }
 
   hasPermissionToRun(): boolean {
     return true;
@@ -29,34 +29,49 @@ export class TopCommand implements Command {
       title: 'Leaderboard - ' + days + ' days',
       color: this.randomColor(),
       // eslint-disable-next-line @typescript-eslint/camelcase
-      // author: { name:  message.author.id, icon_url: message.author.avatarURL },
+      author: { name: this.getName(message.author, message.author.id), icon_url: message.author.avatarURL },
       description: 'Loading...'
     };
 
     const msg = await message.channel.send({ embed });
 
-   await this.getTopDays(days).then(async (top) =>{
+    await this.getTopDays(days).then(async (top) => {
 
-    embed.description = top.map((p, i) =>
-      `${i + 1}) <@${p.userId}> has ${p.total} reputation`
-    ).join('\n');
+      embed.description = top.map((p, i) =>
+        `${i + 1}) <@${p.userId}> has ${p.total} reputation`
+      ).join('\n');
 
-    return await msg.edit({ embed });
+      return await msg.edit({ embed });
     }).catch((err) => {
-    console.error(err);
-     msg.delete();
-     message.react('❌');
-     return null;
-   })
+      console.error(err);
+      msg.delete();
+      message.react('❌');
+      return null;
+    })
   }
 
-  private async getTopDays(days: number): Promise<{userId: string; total: number}[]>{
+  private async getTopDays(days: number): Promise<{ userId: string; total: number }[]> {
     try {
       const { data } = await axios(config.apiUrl + '/points/top/' + days, { method: 'GET', headers: { 'Content-Type': 'application/json' } });
 
-	return data.data;
+      return data.data;
     } catch (e) {
       return Promise.reject(e);
+    }
+  }
+
+  private getName = (user: any, id: string): string => {
+    if (user !== undefined) {
+      // If the user has a 'user' field (read: is a member), return the nickname or user.username. Otherwise, return the user.username.
+
+      // NOTE: We do some weird parsing here, ignore this
+      return Object.prototype.hasOwnProperty.call(user, 'user')
+        ? user.nickname
+          ? user.nickname
+          : user.user.username
+        : user.username
+    } else {
+      return `<@${id}>`
     }
   }
 
